d1a16d7cf900c583688e9f82c7917a2ff4a00fb9
webianks <webianks@gmail.com>
2017-03-19 15:56:02 +0530

    Created table customers

--- app/src/main/java/com/webianks/anotech/MainActivity.java
+++ app/src/main/java/com/webianks/anotech/MainActivity.java
@@ -1,7 +1,10 @@
 package com.webianks.anotech;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.webianks.anotech.database.AnotechDBHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AnotechDBHelper dbHelper = new AnotechDBHelper(this);
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+
+
+        String query = "CREATE INDEX salesRepEmployeeNumber ON customers(salesRepEmployeeNumber)";
+
+        //sqLiteDatabase.execSQL(query);
+
     }
 }
--- app/src/main/java/com/webianks/anotech/database/AnotechDBHelper.java
+++ app/src/main/java/com/webianks/anotech/database/AnotechDBHelper.java
@@ -12,7 +12,7 @@ public class AnotechDBHelper extends SQLiteOpenHelper {
 
 
     public static final String DB_NAME = "anotech.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 5;
 
     public AnotechDBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -21,10 +21,32 @@ public class AnotechDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        String query = "\n" +
+                "CREATE TABLE customers (\n" +
+                "   customerNumber int(11) NOT NULL,\n" +
+                "   customerName varchar(50) NOT NULL,\n" +
+                "   contactLastName varchar(50) NOT NULL,\n" +
+                "   contactFirstName varchar(50) NOT NULL,\n" +
+                "   phone varchar(50) NOT NULL,\n" +
+                "   addressLine1 varchar(50) NOT NULL,\n" +
+                "   addressLine2 varchar(50) DEFAULT NULL,\n" +
+                "   city varchar(50) NOT NULL,\n" +
+                "   state varchar(50) DEFAULT NULL,\n" +
+                "   postalCode varchar(15) DEFAULT NULL,\n" +
+                "   country varchar(50) NOT NULL,\n" +
+                "   salesRepEmployeeNumber int(11) DEFAULT NULL,\n" +
+                "   creditLimit decimal(10,2) DEFAULT NULL,\n" +
+                "   PRIMARY KEY (customerNumber),\n" +
+                "   CONSTRAINT customers_ibfk_1 FOREIGN KEY (salesRepEmployeeNumber) REFERENCES employees (employeeNumber)\n" +
+                ");";
+
+        sqLiteDatabase.execSQL(query);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS customers");
+        onCreate(sqLiteDatabase);
     }
 }
