79b1ca1ec0085daadc83202b31f68360433cbaa3
webianks <webianks@gmail.com>
2017-03-22 23:39:18 +0530

    Loader init

--- app/src/main/java/com/webianks/anotech/screens/StructureViewer.java
+++ app/src/main/java/com/webianks/anotech/screens/StructureViewer.java
@@ -2,8 +2,11 @@ package com.webianks.anotech.screens;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.webianks.anotech.R;
 
@@ -11,7 +14,9 @@ import com.webianks.anotech.R;
  * Created by R Ankit on 22-03-2017.
  */
 
-public class StructureViewer extends AppCompatActivity {
+public class StructureViewer extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Object> {
+
+    private final int STRUCTURE_LOADER = 123;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,9 +29,42 @@ public class StructureViewer extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getSupportLoaderManager().initLoader(STRUCTURE_LOADER, null, this);
+
+    }
+
     private void init() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Object> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Object> loader, Object data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Object> loader) {
+
+    }
+
 }
