2adf1a98739cdf8667f11c061e4291d74aaea7ca
webianks <webianks@gmail.com>
2017-03-22 17:37:46 +0530

    Added new orderDetails class

---
+++ app/src/main/java/com/webianks/anotech/test_classes/OrderDetails.java
@@ -0,0 +1,22 @@
+package com.webianks.anotech.test_classes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.webianks.anotech.R;
+
+/**
+ * Created by R Ankit on 22-03-2017.
+ */
+
+public class OrderDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.order_details);
+
+    }
+}
