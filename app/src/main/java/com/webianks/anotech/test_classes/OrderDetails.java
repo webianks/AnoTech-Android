ec2bd916783551bc88ac01437d81afd1b65d9c5f
webianks <webianks@gmail.com>
2017-03-23 22:30:29 +0530

    order_details insert init

--- app/src/main/java/com/webianks/anotech/test_classes/OrderDetails.java
+++ app/src/main/java/com/webianks/anotech/test_classes/OrderDetails.java
@@ -2,9 +2,12 @@ package com.webianks.anotech.test_classes;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.webianks.anotech.R;
 
@@ -12,7 +15,14 @@ import com.webianks.anotech.R;
  * Created by R Ankit on 22-03-2017.
  */
 
-public class OrderDetails extends AppCompatActivity {
+public class OrderDetails extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputEditText orderNumnberET;
+    private TextInputEditText productCodeET;
+    private TextInputEditText quantityOrderedET;
+    private TextInputEditText priceEachET;
+    private TextInputEditText orderLineNumberET;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +39,16 @@ public class OrderDetails extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        orderLineNumberET = (TextInputEditText) findViewById(R.id.orderLineNumber);
+        orderNumnberET = (TextInputEditText) findViewById(R.id.orderNumber);
+        productCodeET = (TextInputEditText) findViewById(R.id.productCode);
+        quantityOrderedET = (TextInputEditText) findViewById(R.id.quantityOrdered);
+        priceEachET = (TextInputEditText) findViewById(R.id.priceEach);
+
+        findViewById(R.id.insert).setOnClickListener(this);
+
     }
 
 
@@ -39,5 +59,17 @@ public class OrderDetails extends AppCompatActivity {
             finish();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int order_line_number = Integer.valueOf(orderLineNumberET.getText().toString());
+        int order_number = Integer.valueOf(orderNumnberET.getText().toString());
+        int quantity_ordered = Integer.valueOf(quantityOrderedET.getText().toString());
+        int price_each = Integer.valueOf(priceEachET.getText().toString());
+        String product_code = productCodeET.getText().toString();
+
+
     }
 }
