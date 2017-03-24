961f126407a08eef0a177aa7d1da7096e25f7943
webianks <webianks@gmail.com>
2017-03-25 00:56:02 +0530

    Orders logic init

--- app/src/main/java/com/webianks/anotech/test_classes/Orders.java
+++ app/src/main/java/com/webianks/anotech/test_classes/Orders.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.webianks.anotech.R;
 
@@ -12,13 +13,13 @@ import com.webianks.anotech.R;
  * Created by R Ankit on 24-03-2017.
  */
 
-public class Orders extends AppCompatActivity {
+public class Orders extends AppCompatActivity implements View.OnClickListener {
 
 
-    private TextInputEditText orderNumnberET;
+    private TextInputEditText orderNumberET;
     private TextInputEditText orderDateET;
     private TextInputEditText requiredDateET;
-    private TextInputEditText shppedDateET;
+    private TextInputEditText shippedDateET;
     private TextInputEditText statusET;
     private TextInputEditText commentsET;
     private TextInputEditText customerNumberET;
@@ -38,6 +39,31 @@ public class Orders extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        orderNumberET = (TextInputEditText) findViewById(R.id.orderNumber);
+        orderDateET = (TextInputEditText) findViewById(R.id.orderDate);
+        requiredDateET = (TextInputEditText) findViewById(R.id.requiredDate);
+        shippedDateET = (TextInputEditText) findViewById(R.id.shippedDate);
+        statusET = (TextInputEditText) findViewById(R.id.status);
+        commentsET = (TextInputEditText) findViewById(R.id.comments);
+        customerNumberET = (TextInputEditText) findViewById(R.id.customerNumber);
+
+        findViewById(R.id.insert).setOnClickListener(this);
+        findViewById(R.id.run_test).setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.insert)
+            insertNow();
+        //else
+            //runCheck();
+    }
+
+
+    private void insertNow() {
+
+    }
 }
