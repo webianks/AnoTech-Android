9d534876127926d45526519f284274a51a4fd69d
webianks <webianks@gmail.com>
2017-03-25 00:50:31 +0530

    Orders layout design done

--- app/src/main/java/com/webianks/anotech/test_classes/Orders.java
+++ app/src/main/java/com/webianks/anotech/test_classes/Orders.java
@@ -2,7 +2,9 @@ package com.webianks.anotech.test_classes;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.webianks.anotech.R;
 
@@ -12,12 +14,30 @@ import com.webianks.anotech.R;
 
 public class Orders extends AppCompatActivity {
 
+
+    private TextInputEditText orderNumnberET;
+    private TextInputEditText orderDateET;
+    private TextInputEditText requiredDateET;
+    private TextInputEditText shppedDateET;
+    private TextInputEditText statusET;
+    private TextInputEditText commentsET;
+    private TextInputEditText customerNumberET;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.orders);
 
-
+        init();
     }
+
+    private void init() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
 }
