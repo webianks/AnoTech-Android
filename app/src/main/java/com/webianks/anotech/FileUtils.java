f175ffb3bb14f681f00486f62af5185523d5958b
webianks <webianks@gmail.com>
2017-03-26 00:12:41 +0530

    writing csv file of date_difference

---
+++ app/src/main/java/com/webianks/anotech/FileUtils.java
@@ -0,0 +1,61 @@
+package com.webianks.anotech;
+
+import android.os.Environment;
+import android.text.format.DateFormat;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by R Ankit on 22-02-2017.
+ */
+
+public class FileUtils {
+
+    private static File filepath;
+
+    public static boolean writeOutputFile(String content) {
+
+        BufferedWriter bw = null;
+        boolean success = false;
+
+        try {
+            bw = new BufferedWriter(new FileWriter(filepath, true));
+            bw.write(content);
+            bw.newLine();
+            bw.flush();
+            success = true;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            success = false;
+        } finally {
+            if (bw != null) try {
+                bw.close();
+            } catch (IOException ioe2) {
+            }
+        }
+        return success;
+    }
+
+    public static void createOutputFile(){
+
+        File root = new File(Environment.getExternalStorageDirectory(), "Anotech");
+        if (!root.exists()) {
+            root.mkdirs();
+        }
+        filepath = new File(root,"orders_date_difference.csv");
+    }
+
+    public static void deleteFile(){
+
+        if (filepath!=null){
+            boolean deleted = filepath.delete();
+            if (deleted)
+                Log.d("webi","Deleted older file.");
+        }
+    }
+
+}
--- app/src/main/java/com/webianks/anotech/test_classes/Orders.java
+++ app/src/main/java/com/webianks/anotech/test_classes/Orders.java
@@ -1,5 +1,6 @@
 package com.webianks.anotech.test_classes;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -13,15 +14,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.webianks.anotech.FileUtils;
 import com.webianks.anotech.R;
 import com.webianks.anotech.database.AnotechDBHelper;
 import com.webianks.anotech.database.Contract;
 import com.webianks.anotech.database.Projections;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by R Ankit on 24-03-2017.
@@ -77,28 +85,62 @@ public class Orders extends AppCompatActivity implements View.OnClickListener {
     }
 
 
+    @SuppressLint("WrongConstant")
     private void runCheck() {
 
         AnotechDBHelper dbHelper = new AnotechDBHelper(this);
         SQLiteDatabase database = dbHelper.getReadableDatabase();
-/*
 
-        String query = "SELECT orderNumber, datediff(shippedDate,orderDate) FROM " + Contract.TABLE_ORDERS + "WHERE " +
-                "NOT shippedDate ='' AND orderNumber != 10165;";
-
-        database.execSQL(query);
-*/
-
-        String[] selectionArgs = {"10165"};
+        String[] selectionArgs = {"10165", ""};
 
         String selection = Contract.TABLE_ORDERS +
-                "." + Contract.OrdersEntry.ORDER_NUMBER + " != ? ";
+                "." + Contract.OrdersEntry.ORDER_NUMBER + " != ? AND " +
+                Contract.TABLE_ORDERS + "." + Contract.OrdersEntry.SHIPPED_DATE + " != ?";
 
         Cursor cursor = database.query(Contract.TABLE_ORDERS, Projections.ORDERS_COLUMNS,
                 selection, selectionArgs, null, null, null);
 
-        Log.d(Orders.class.getSimpleName()," "+cursor.getCount());
+        SimpleDateFormat myFormat = new SimpleDateFormat("YYYY-MM-DD");
+        StringBuilder stringBuilder = new StringBuilder();
 
+        while (cursor.moveToNext()) {
+
+            int order_number_index = cursor.getColumnIndex(Contract.OrdersEntry.ORDER_NUMBER);
+            int shipped_date_index = cursor.getColumnIndex(Contract.OrdersEntry.SHIPPED_DATE);
+            int order_date_index = cursor.getColumnIndex(Contract.OrdersEntry.ORDER_DATE);
+
+            String orderNumber = cursor.getString(order_number_index);
+            String orderDate = cursor.getString(order_date_index);
+            String shippedDate = cursor.getString(shipped_date_index);
+
+            String[] splittedOrderDate = orderDate.split("-");
+            String[] splittedShippedDate = shippedDate.split("-");
+
+            Calendar calender = Calendar.getInstance();
+            calender.set(Calendar.DAY_OF_MONTH, Integer.valueOf(splittedOrderDate[2]));
+            calender.set(Calendar.MONTH, Integer.valueOf(splittedOrderDate[1]));
+            calender.set(Calendar.YEAR, Integer.valueOf(splittedOrderDate[0]));
+
+            Calendar calender2 = Calendar.getInstance();
+            calender2.set(Calendar.DAY_OF_MONTH, Integer.valueOf(splittedShippedDate[2]));
+            calender2.set(Calendar.MONTH, Integer.valueOf(splittedShippedDate[1]));
+            calender2.set(Calendar.YEAR, Integer.valueOf(splittedShippedDate[0]));
+
+            long diff = calender2.getTimeInMillis() - calender.getTimeInMillis();
+            long days = diff / (24 * 60 * 60 * 1000);
+
+            //Log.d(Orders.class.getSimpleName(),"OrderNumber: "+orderNumber+" -- Days: " + days);
+
+            stringBuilder.append(orderNumber + "," + days + "\n");
+
+            FileUtils.createOutputFile();
+
+            if (FileUtils.writeOutputFile(stringBuilder.toString()))
+                Log.d(Orders.class.getSimpleName(), "Writing csv file done.");
+            
+        }
+
+        Log.d(Orders.class.getSimpleName(), " " + cursor.getCount());
 
     }
 
@@ -138,6 +180,7 @@ public class Orders extends AppCompatActivity implements View.OnClickListener {
 
             long code = database.insert(Contract.TABLE_ORDERS, null, contentValues);
 
+
             if (code > 0)
                 Toast.makeText(this, getString(R.string.done), Toast.LENGTH_LONG).show();
             else
@@ -158,4 +201,6 @@ public class Orders extends AppCompatActivity implements View.OnClickListener {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
